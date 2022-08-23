066691407b46368fc2103dda8dbc039188dde055
Didi nGon- <ngon-@hotmail.com>
2022-08-23 19:58:22 +0100

    Add grid size change button

--- etch.js
+++ etch.js
@@ -1,16 +1,33 @@
 const container = document.querySelector('#container');
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel')
-        pixel.addEventListener('mouseover', e => pixelEventListener(e))
-        row.appendChild(pixel);
+let gridSize = 16;
+function updateGrid() {
+    container.replaceChildren();
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < gridSize; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel')
+            pixel.addEventListener('mouseover', e => pixelEventListener(e))
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
 function pixelEventListener(e) {
-    e.target.classList.add('pixel-colored')
+    e.target.classList.add('pixel-colored');
 }
+
+function promptGridSize() {
+    let valid = false;
+    do {
+        inp = prompt('Enter grid size (1-100)');
+        if (inp >= 1 && inp <= 100) {
+            valid = true;
+        }
+    } while (!valid);
+    gridSize = Math.floor(inp);
+    updateGrid();
+}
+updateGrid();
