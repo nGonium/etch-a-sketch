bf1407968176397dc9aeb244d0e2217bd4f7da11
Didi nGon- <ngon-@hotmail.com>
2022-08-23 19:35:32 +0100

    Add sketch function

--- etch.js
+++ etch.js
@@ -5,7 +5,12 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel')
+        pixel.addEventListener('mouseover', e => pixelEventListener(e))
         row.appendChild(pixel);
     }
     container.appendChild(row);
+}
+
+function pixelEventListener(e) {
+    e.target.classList.add('pixel-colored')
 }
